800af651b66a03b6ed55d36a37caace0a6f8a67f
Abhijit <abhijitthander38@gmail.com>
2026-01-03 09:21:04 +0530

    moduling system

---
+++ Node/moduling/globals.js
@@ -0,0 +1,9 @@
+// __dirname
+console.log(__dirname); // It will print the current directory path
+// console.log(module); // it will print the module object
+// console.log(__filename); //it will print the current file path
+// console.log(process); // it will prin tthe process object(Prpcess.env)
+
+// console.log(require) //it will print the require function   
+
+console.log(global)
