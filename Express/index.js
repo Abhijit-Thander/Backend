95c0b4d0379b0e8f1c5fe80ba7a499ea7e5c06d6
Abhijit <abhijitthander38@gmail.com>
2026-01-05 19:12:40 +0530

    optimize route

--- Express/index.js
+++ Express/index.js
@@ -3,11 +3,28 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.get("/home", (req, res) => {
-  res.send("Welcome to home page");
-});
+// app.get("/", (req, res) => {
+//   res.send("This is home page");
+// });
+// app.get("/products", (req, res) => {
+//   res.send("Show all products");
+// });
+// app.post("/products", (req, res) => {
+//   res.send("Create a new product");
+// });
+// app.put("/products", (req, res) => {
+//   res.send("Update a  product");
+// });
+
+// app.delete("/products", (req, res) => {
+//   res.send("Delete a  product");
+// });
+
+app
+  .route("/products")
+  .get((req, res) => res.send("Show all products"))
+  .post((req, res) => res.send("Create a new product"))
+  .put((req, res) => res.send("Update a  product"))
+  .delete((req, res) => res.send("Delete a  product"));
 
 app.listen(port, () => console.log(`Server on listening on port:${port}`));
