87f36e21017d4055594d91a8e2088d1c52e6b0d4
Abhijit <abhijitthander38@gmail.com>
2026-01-06 19:10:13 +0530

    params

--- Express/app.js
+++ Express/app.js
@@ -3,10 +3,20 @@ import students from "./routes/students.js";
 const app = express();
 const port = 3000;
 
-app.use("/students", students);
+// app.use("/students", students);
 
 app.get("/", (req, res) => {
   res.send("This is Home");
+});
+
+//----------------------- Route Parameters -----------------------//
+
+app.get("/products", (req, res) => {
+  res.send(`<h1>Show all products</h1>`);
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`<h1>${req.params.id}</h1>`);
 });
 
 app.listen(port, () => {
