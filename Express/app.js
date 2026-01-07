385ceae5137d8075113fd3939ace0ef431e68401
Abhijit <abhijitthander38@gmail.com>
2026-01-07 18:56:19 +0530

    query string

--- Express/app.js
+++ Express/app.js
@@ -11,12 +11,28 @@ app.get("/", (req, res) => {
 
 //----------------------- Route Parameters -----------------------//
 
-app.get("/products", (req, res) => {
-  res.send(`<h1>Show all products</h1>`);
+// app.get("/products", (req, res) => {
+//   res.send(`<h1>Show all products</h1>`);
+// });
+
+// app.get("/products/:id", (req, res) => {
+//   res.send(`<h1>${req.params.id}</h1>`);
+// });
+
+app.get("/profile/:name", (req, res) => {
+  const name = req.params.name || "Guest";
+  res.send(`Name is ${name}`);
 });
 
-app.get("/products/:id", (req, res) => {
-  res.send(`<h1>${req.params.id}</h1>`);
+// app.all("*", (req, res) => {
+//   res.status(404).send("<h1>404! Page Not Found</h1>");
+// });
+
+// ------------ Query string-----------------
+app.get("/products", (req, res) => {
+  const {category, color} = req.query;
+  // res.send(`Category is ${req.query.category} & color is ${req.query.color}`);
+  res.send(`Category is ${category} & color is ${color}`);
 });
 
 app.listen(port, () => {
