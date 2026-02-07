8e85403414e4bf3817a8170040d980ce87c6a39e
Abhijit <abhijitthander38@gmail.com>
2026-02-07 21:30:18 +0530

    first jest testing

---
+++ Express/tests/demo.test.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  return a + b;
+}
+
+test("sum of 1 & 2 should be 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("sum of 4 & 6 should not be 3", () => {
+  expect(sum(4, 6)).not.toBe(3);
+});
